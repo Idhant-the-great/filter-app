9ac3be14e609a13be120a99531e2822b5f4f6da5
Idhant-the-great <82503254+Idhant-the-great@users.noreply.github.com>
2022-01-30 10:40:28 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+function preload(){
+}
+
+function setup()
+{
+    canvas= createCanvas(300,300);
+    
+}
+
+function draw(){
+canvas.center()
+}
+function take_snapshot()
+{
+    save('myFilterImage.png');
+}
